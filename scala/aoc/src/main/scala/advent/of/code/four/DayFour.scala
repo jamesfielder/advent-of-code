56c70cd9442f0a28cd70b9c348fec5ba56487dfc
James Fielder <james@fielder.dev>
2020-12-06 19:20:59 +0000

    Day four solution
    Definitely naughty as I didn't write any tests!

---
+++ scala/aoc/src/main/scala/advent/of/code/four/DayFour.scala
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2020 James Fielder.
+ * All rights reserved.
+ */
+
+package advent.of.code.four
+
+import advent.of.code.utils.Utils
+import cats.effect.ExitCode
+import monix.eval.{Task, TaskApp}
+
+object DayFour extends TaskApp with Utils {
+
+  import Validators._
+
+  val required = Seq("ecl", "pid", "iyr", "eyr", "hcl", "byr", "hgt")
+
+  override def run(args: List[String]): Task[ExitCode] =
+    (for {
+      _ <- part1
+      _ <- part2
+    } yield ()).as(ExitCode.Success)
+
+  def part1 =
+    lines
+      .filter(part1Validator)
+      .compile.toList.map(p => println("part 1: " + p.size))
+
+  def part2 =
+    lines
+      .filter(validatePassword)
+      .compile.toList.map(p => println("part 2: " + p.size))
+
+  def part1Validator(passport: Map[String, String]): Boolean =
+    required.forall(passport.keys.toList.contains(_))
+
+  private def lines = readFileStream("files/4.txt")
+    .split(_.isBlank)
+    .map(_.foldLeft("")((a, b) => a + " " + b))
+    .map(chunkToMap)
+
+  private def chunkToMap(c: String): Map[String, String] = {
+    c.trim.split(" ").toList.map { s =>
+      s.split(":").toList match {
+        case k :: v :: Nil => k -> v
+        case _ => throw new IllegalArgumentException(s"String $s was not parsable for this challenge")
+      }
+    }.toMap
+  }
+}
+
+object Validators {
+
+  type Validator = String => Boolean
+
+  def validatePassword(password: Map[String, String]): Boolean = {
+    val filtered = password.filter(p => part2Validator.keys.toList.contains(p._1))
+
+    filtered.keys.size == 7 && filtered.forall { case (k, v) => part2Validator(k).apply(v) }
+  }
+
+  val part2Validator = Map[String, Validator](
+    "byr" -> fieldBetween(1920, 2002),
+    "iyr" -> fieldBetween(2010, 2020),
+    "eyr" -> fieldBetween(2020, 2030),
+    "hgt" -> allowedHeight,
+    "hcl" -> allowedHairColour,
+    "ecl" -> allowedEyeColour,
+    "pid" -> allowedPassportId
+  )
+
+  def fieldBetween(lower: Int, upper: Int): Validator = (s: String) => s.toInt.inside(lower, upper)
+
+  def allowedHeight: Validator = {
+    case s"${num}cm" => fieldBetween(150, 193).apply(num)
+    case s"${num}in" => fieldBetween(59, 76).apply(num)
+    case _ => false
+  }
+
+  def allowedHairColour: Validator = {
+    case s"#${hc}" => hc.length == 6 && hc.allowedChars
+    case _ => false
+  }
+
+  def allowedEyeColour: Validator = (e: String) => Seq("amb", "blu", "brn", "gry", "grn", "hzl", "oth").contains(e)
+
+  def allowedPassportId: Validator = (p: String) => p.length == 9 && p.allowedNumbersOnly
+
+  implicit class RangeValidator(i: Int) {
+    def inside(lower: Int, upper: Int): Boolean = i >= lower && i <= upper
+  }
+
+  implicit class CharacterValidator(s: String) {
+    val allowed: Seq[Char] = ('a' to 'f') ++ ('0' to '9')
+    val numbers: Seq[Char] = '0' to '9'
+
+    def allowedChars: Boolean = s.toList.forall(allowed.contains(_))
+
+    def allowedNumbersOnly: Boolean = s.toList.forall(numbers.contains(_))
+  }
+
+}
--- scala/aoc/src/main/scala/advent/of/code/three/teststreams.sc
+++ scala/aoc/src/main/scala/advent/of/code/three/teststreams.sc
@@ -1,3 +1,38 @@
 import fs2._
+import advent.of.code.utils.Utils
+import cats.effect.ExitCode
+import monix.eval.{Task, TaskApp}
+
+import scala.collection.View.Empty.toMap
 
 Stream(1, 2, 3).chunkN(2, allowFewer = false).take(2).map(_.last).collect{ case Some(i) => i}.toList
+
+object Tester extends TaskApp with Utils {
+  override def run(args: List[String]): Task[ExitCode] = ???
+
+  def readFour = readFileStream("C:\\Users\\jcfie\\code\\advent-of-code\\scala\\files\\4.txt")
+}
+
+import monix.execution.Scheduler.Implicits.global
+
+val test = ("eyr:2039".split(":").toList match {
+  case k :: v :: Nil => k -> v
+})
+
+Map[String, String]() + ("a" -> "b")
+
+
+Tester.readFour
+  .split(_.isBlank)
+//  .debug()
+//  .evalTap(o => Task { println(o.foldLeft("")((a, b) => a + b)) })
+//  .map(cs => cs.foldLeft(Map[String, String]()){ (m, s) =>
+//    val split: (String, String) = s.split(":").toList match { case k :: v :: Nil => k -> v}
+//    m + split
+//  })
+
+  .compile.toList.runSyncUnsafe()
+  .foreach(println(_))
+//  println("++++")
+//  println(l)
+//})
